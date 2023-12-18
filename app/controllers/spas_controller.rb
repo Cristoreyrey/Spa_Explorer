3dfa97a1a5503a7048fa034307d36669ed88d483
Cristobal Diaz <escalanteciudades@gmail.com>
2023-12-18 17:03:23 -0600

    crear controlador de spas y crear página de hhtml HOME, se añadieron scss para esa pagina

--- app/controllers/spas_controller.rb
+++ app/controllers/spas_controller.rb
@@ -1,6 +1,45 @@
 class SpasController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: :home
+
+
+def home
+  @spas = Spa.all
+end
+
 
   def index
+    @spas = Spa.all
   end
+
+  def show
+    @spa = Spa.find(params[:id])
+  end
+
+
+  def new
+    @spa = Spa.new
+  end
+
+  def create
+    @spa = Spa.new(spa_params)
+    @spa.save
+    redirect_to spa_path(@spa)
+
+  end
+
+
+
+  def spa_params
+    params.require(:spa).permit(:name, :address, :price, :rating)
+  end
+
+
+  def destroy
+    @spa = Spa.find(params[:id])
+    @spa.destroy
+
+    redirect_to root_path notice: 'Your Spa was successfully destroyed.'
+  end
+
+
 end
