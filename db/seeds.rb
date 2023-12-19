5ed26c991836be998a1d39c7f9112d589b800fa9
Cristobal Diaz <escalanteciudades@gmail.com>
2023-12-19 11:51:57 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+  user = User.new(
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    role: ['host', 'guest'].sample
+  )
+
+  unless user.save
+    puts user.errors.full_messages
+  end
+end
