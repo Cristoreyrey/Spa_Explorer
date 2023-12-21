5bed8f48cc1c3882bf1dde2407e3619f6095e36d
Takahashi-Seiji <takahashiseiji18@gmail.com>
2023-12-21 14:32:22 -0600

    Map Search Engine part2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def dashboard
+    @spas = Spa.all
+    if params[:query].present?
+        @spas = Spa.search_by_name_and_address(params[:query])
+    end
   end
 end
