04ee4173ed916a416c18757dc7fba0cb033be6ae
Takahashi-Seiji <takahashiseiji18@gmail.com>
2023-12-22 16:08:05 -0600

    Seed try :(

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-10.times do
-  user = User.new(
-    email: Faker::Internet.unique.email,
-    password: 'password',
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    role: ['host', 'guest'].sample
-  )
+require 'faker'
 
-  unless user.save
-    puts user.errors.full_messages
-  end
-end
+# users = 5.times do
+#   User.create!(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password,
+#     role: 'host'
+#   )
+# end
+
+# userr.each do |user|
+#   2.times do
+#     user.spas.create!(
+#       name: Faker::Company.name,
+#       location: Faker::Address.street_address,
+#       price: Faker::Number.decimal(2),
+#       rating: rand(1..5)
+#       3.times do
+#         spa.images.attach(io:
+#       end
+#     )
+#   end
+# end
